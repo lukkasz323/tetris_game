1245b2349a2e2c1dba942df8a2d88d6b49c530f1
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-07-10 23:08:36 +0200

    Prepare core pygame structure

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pygame
 
 def main():
-   pygame.init()
+    WIDTH, HEIGHT = 512, 512
+    
+    pygame.init()
+    
+    W = pygame.display.set_mode((WIDTH, HEIGHT))
+    
+    run = True
+    while run:
+        for event in pygame.event.get():
+            match event.type:
+                case pygame.QUIT:
+                    run = False
+                    
+    pygame.quit()
 
 if __name__ == '__main__':
     main()
