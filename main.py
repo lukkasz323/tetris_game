81308e94cb8577c56b6bc02135d10616b6bec4c8
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-07-11 00:04:23 +0200

    Add clock, background / grid drawing, change WIDTH

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import pygame
 
 def main():
-    WIDTH, HEIGHT = 512, 512
+    FRAMERATE = 60
+    WIDTH, HEIGHT = 320, 512
+    GRID = 32
     
     pygame.init()
     
+    clock = pygame.time.Clock()
+    
     W = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('Tetris')
     
     run = True
     while run:
+        clock.tick(FRAMERATE)
+        
         for event in pygame.event.get():
             match event.type:
                 case pygame.QUIT:
                     run = False
+                    
+        W.fill((16, 16, 16))
+        
+        # Draw grid.
+        for x in range(0, WIDTH, GRID):
+            for y in range(0, HEIGHT, GRID):
+                rect = pygame.Rect(x, y, GRID, GRID)
+                pygame.draw.rect(W, 'gray10', rect, 1)
+        
+        # Update screen.
+        pygame.display.update()
                     
     pygame.quit()
 
